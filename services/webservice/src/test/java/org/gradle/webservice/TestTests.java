31ee3200c30b52f0ad646be93ce1e03593ce34a8
yucaci24 <39161582+yucaci24@users.noreply.github.com>
2019-03-14 15:48:19 -0500

    Create TestTests.java

---
+++ services/webservice/src/test/java/org/gradle/webservice/TestTests.java
@@ -0,0 +1,13 @@
+package org.gradle.webservice;
+
+import junit.framework.TestCase;
+
+public class TestTestTest1 extends TestCase {
+    public void testClasspath1() {
+        new TestTest().method();
+    }
+
+    public void testApiCompileClasspath1() {
+        new org.gradle.api.PersonList();
+    }
+}
