2a695f44654d75d6d5ff2e0ffbfe64fbe3709f79
yucaci24 <39161582+yucaci24@users.noreply.github.com>
2019-08-22 11:04:06 -0500

    Update PersonTest.java

--- shared/src/test/java/org/gradle/shared/PersonTest.java
+++ shared/src/test/java/org/gradle/shared/PersonTest.java
@@ -20,6 +20,6 @@ public class PersonTest extends TestCase {
     public void testTestProperty() throws IOException {
         Properties properties = new Properties();
         properties.load(getClass().getClassLoader().getResourceAsStream("org/gradle/shared/test.properties"));
-        assertEquals("testValue", properties.getProperty("test"));
+        assertEquals("testVal", properties.getProperty("test"));
     }
 }
